16b171a856dfac6573568e91a97e2c896633f26b
amiekhb <aminaaami0715@gmail.com>
2024-06-10 12:49:30 +0800

    first commit

---
+++ js_basics/main.js
@@ -0,0 +1,71 @@
+//1
+// let ner = "Amie"; //string
+// let nas = 20; // number 20, 20.30
+// let gerleseneseh = false; //boolean: true/false
+//2
+let age = 18;
+console.log("нас:", age);
+//3
+let firstname = "Amin-Erdene";
+let lastname = "Khartsaga";
+let country = "Mongolia";
+let email = "aminaaami0715@gmail.com";
+console.log("нэр:", firstname);
+console.log("овог:", lastname);
+console.log("улс:", country);
+console.log("mail:", email);
+//4
+let ner = "Amie";
+console.log("turul:", typeof ner);
+let nas = 20;
+console.log("turul:", typeof nas);
+let gerleseneseh = false;
+console.log("turul:", typeof gerleseneseh);
+//5
+let number1 = 2;
+number2 = 5;
+let x = number1 + number2;
+console.log("hariu1:", x);
+let y = number2 - number1;
+console.log("hariu2:", y);
+let z = number1 * number2;
+console.log("hariu3:", z);
+let w = number2 / number1;
+console.log("hariu4:", w);
+//6
+let a = 3;
+b = 7;
+c = 9;
+let p = a + b + c;
+console.log("p:", p);
+//7
+let gb = 15;
+bt = 1e9;
+let hariuu = 15 * 1e9;
+console.log("bytes:", hariuu);
+//8
+let celcius = 30;
+let faranheit = (celcius * 9) / 5 + 32;
+console.log("hem:", faranheit);
+//9
+let faranheitt = 100;
+let celciuss = ((faranheitt - 32) * 5) / 9;
+console.log("hemm:", celciuss);
+//10
+let utga1 = 10;
+utga2 = 15;
+let nemeh = utga1 + utga2;
+console.log("nem:", nemeh);
+let hasah = utga2 - utga1;
+console.log("has:", hasah);
+let urjih = utga1 * utga2;
+console.log("urj:", urjih);
+let huvaah = utga1 / utga2;
+console.log("huvaa:", huvaah);
+let huvi = utga2 % utga1;
+console.log("huvi:", huvi);
+//11
+let inch = 48;
+cm = 2.54;
+let total_cm = inch * cm;
+console.log("total_cm:", total_cm);
