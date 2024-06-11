4d9289f12cee8fc4bfa775780ae6e06bef7244bf
amiekhb <aminaaami0715@gmail.com>
2024-06-11 13:02:15 +0800

    conditionals

---
+++ js_conditionals/main.js
@@ -0,0 +1,111 @@
+// let x = 0;
+// console.log("Garaas avsan utga:", x);
+// console.log("Ugugdliin turul:", typeof x);
+
+// let week_number = Number(prompt("Та 1-7 хооронд тоо оруулна уу !!!"));
+// let garagiin_ner;
+// if (week_number === 1) {
+//   //   console.log("Даваа");
+//   garagiin_ner = "Даваа";
+// } else if (week_number === 2) {
+//   //   console.log("Мягмар");
+//   garagiin_ner = "Мягмар";
+// } else if (week_number === 3) {
+//   //   console.log("Лхагва");
+//   garagiin_ner = "Лхагва";
+// } else if (week_number === 4) {
+//   //   console.log("Пүрэв");
+//   garagiin_ner = "Пүрэв";
+// } else if (week_number === 5) {
+//   //   console.log("Баасан");
+//   garagiin_ner = "Баасан";
+// } else if (week_number === 6) {
+//   //   console.log("Бямба");
+//   garagiin_ner = "Бямба";
+// } else if (week_number === 7) {
+//   //   console.log("Ням");
+//   garagiin_ner = "Ням";
+// } else {
+//   console.log("oldsongui");
+// }
+// console.log("tanii oruulsan too:", week_number);
+// console.log("Tanii oruulsan toond tohiroh utga:", garagiin_ner);
+
+// let age = 16;
+// let gender = "er";
+// if (age >= 18 && gender === "er") {
+//   console.log("Ta nasandhursen eregtei baina:");
+// } else {
+//   console.log(" ta nasand hureegui esvel emegtei baina:");
+// }
+
+// if (age >= 18 || gender === "er") {
+//   console.log("Ta nasandhursen esvel eregtei baina:");
+// } else {
+//   console.log(" ta nasand hureegui bolon emegtei baina:");
+// }
+// switch (week_number) {
+//   case 1: {
+//     garagiin_ner = "Даваа";
+//     break;
+//   }
+//   case 2: {
+//     garagiin_ner = "Мягмар";
+//     break;
+//   }
+//   case 3: {
+//     garagiin_ner = "Лхагва";
+//     break;
+//   }
+//   case 4: {
+//     garagiin_ner = "Пүрэв";
+//     break;
+//   }
+//   default: {
+//     garagiin_ner = "oldsongui";
+//   }
+// }
+// console.log("tanii oruulsan too:", week_number);
+// console.log("Tanii oruulsan toond tohiroh utga:", garagiin_ner);
+
+// //1
+// let myAge = Number(prompt("Та тоо оруулна уу!"));
+// if (myAge <= 25) {
+//   console.log("tanii nas:", "below 25");
+// } else if (myAge >= 25) {
+//   console.log("tanii nas:", "above 25");
+// }
+
+// //2
+// let n = Number(prompt("Та тоо оруулна уу!!"));
+// // let n;
+// // console.log(typeof n);
+// // console.log(n % 2);
+// if (n % 2 === 0) {
+//   console.log("toonii turul:", "it is even");
+// } else {
+//   console.log("odd");
+
+//3
+let too1 = 12,
+  too2 = 45,
+  too3 = 30;
+if (too1 > too2 && too1 > too3) {
+  console.log("hamgiin ih utga:", too1);
+} else if (too2 > too1 && too2 > too3) {
+  console.log("hamgiin ih utga:", too2);
+} else if (too3 > too1 && too3 > too2) {
+  console.log("hamgiin ih utga:", too3);
+}
+//4
+let too_1 = 12,
+  too_2 = 45,
+  too_3 = 30;
+if (too_1 < too_2 && too_1 < too_3) {
+  console.log("hamgiin baga utga:", too_1);
+} else if (too_2 < too_1 && too_2 < too_3) {
+  console.log("hamgiin baga utga:", too_2);
+} else if (too_3 < too_1 && too_3 < too_2) {
+  console.log("hamgiin baga utga:", too_3);
+}
+//5
